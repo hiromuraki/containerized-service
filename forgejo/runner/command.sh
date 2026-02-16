3a2c355a2dc5f6b82b96436ad3d051cb2709787f
HiroMuraki <419734782@qq.com>
2026-02-16 18:45:54 +0800

    update

---
+++ forgejo/runner/command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ -z "$FORGEJO_RUNNER_REGISTRATION_TOKEN" ]; then
+    echo 'Waiting for Token...';
+    sleep 10;
+    exit 1;
+fi;
+
+if [ ! -f /data/.runner ]; then
+    echo 'First time setup: Registering runner...';
+    forgejo-runner register --no-interactive \
+        --instance "$FORGEJO_INSTANCE_URL" \
+        --token "$FORGEJO_RUNNER_REGISTRATION_TOKEN" \
+        --name home-runner \
+        --config /data/config.yml;
+fi;
+
+echo 'Starting runner daemon...';
+forgejo-runner daemon --config /data/config.yml;
